ccce84651b84fe90a3b4f9cb8c87d63f2a862b7e
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-03-16 21:06:21 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de programação
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
-    return   <div>
-    <h1>Alterando a frase</h1> 
-</div>
+  return (
+    <div>
+      <h1>Alterando a frase</h1>
+    </div>
+  );
 }
 
 export default Home;
