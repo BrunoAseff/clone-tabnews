31564ea06bdc9ca86bdb3c802b9fb833dfb2ed49
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-03-24 21:26:51 +0000

    .

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <img src="MENU.png" alt="Imagem de construção" />
-      <h1>Em construção</h1>
-      <p>menuscore - 2024</p>
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <img
+        src="MENU.png"
+        alt="Imagem de construção"
+        style={{ width: "200px" }}
+      />
+
+      <h1 style={{ marginTop: "20px" }}>Em construção...</h1>
+
+      <p style={{ marginTop: "20px", marginBottom: "50px" }}>
+        Menuscore - 2024
+      </p>
     </div>
   );
 }
