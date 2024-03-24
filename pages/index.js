920b4353fec572af793402316aab32572cbd98da
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-03-24 21:39:06 +0000

    ....

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import React from "react";
+import minhaImagem from "/workspaces/clone-tabnews/MENU.png";
 
 function Home() {
   return (
     <div style={{ textAlign: "center", paddingTop: "50px" }}>
       <img
-        src="workspaces/clone-tabnews/MENU.png"
+        src={minhaImagem}
         alt="Imagem de construção"
         style={{ width: "200px" }}
       />
-
       <h1 style={{ marginTop: "20px" }}>Em construção...</h1>
-
       <p style={{ marginTop: "20px", marginBottom: "50px" }}>
         Menuscore - 2024
       </p>
