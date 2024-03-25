feadbdab3a4d3f201b5dad7d5982adbaba4280ce
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-03-25 13:06:33 +0000

    altera insercao da imagem

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import img1 from "/workspaces/clone-tabnews/src/images/MENU.png";
 
 function Home() {
   return (
     <div style={{ textAlign: "center", paddingTop: "50px" }}>
-      <img src={img1} alt="Imagem de construção" />
+      <img
+        src="/workspaces/clone-tabnews/src/images/MENU.png"
+        alt="Imagem de construção"
+      />
       <h1 style={{ marginTop: "20px" }}>Em construção...</h1>
       <p style={{ marginTop: "20px", marginBottom: "50px" }}>
         Menuscore - 2024
