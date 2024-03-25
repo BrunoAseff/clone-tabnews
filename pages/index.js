fffb67688df7f67012611c939146918aa2201796
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-03-25 12:53:38 +0000

    adiciona imagem na pagina

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import React from "react";
+import img1 from "/workspaces/clone-tabnews/src/images/MENU.png";
 
 function Home() {
   return (
     <div style={{ textAlign: "center", paddingTop: "50px" }}>
-      <img
-        src="/workspaces/clone-tabnews/MENU.png"
-        alt="Imagem de construção"
-        style={{ width: "200px" }}
-      />
+      <img src={img1} alt="Imagem de construção" />
       <h1 style={{ marginTop: "20px" }}>Em construção...</h1>
       <p style={{ marginTop: "20px", marginBottom: "50px" }}>
         Menuscore - 2024
