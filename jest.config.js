2a56193633d7e48622d21b0bfd32472ca540ac7a
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-06-09 16:31:44 +0000

    makes npm test more robust

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const createNextConfig = nextJest({
 
 const jestConfig = createNextConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 });
 
 module.exports = jestConfig;
